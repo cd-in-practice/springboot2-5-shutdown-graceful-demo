52de3f99275c1b8b27dba1606540f0c8bef224e2
zacker330 <zacker330@gmail.com>
2021-05-31 20:56:06 +0800

    init

---
+++ src/test/java/com/example/shutdowngraceful/ShutdownGracefulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shutdowngraceful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShutdownGracefulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
